55921a09e517a2072948198f6182a5cd273ffac1
Nayara Christina Ferreira dos Santos <nayara.christina@totvs.com.br>
2021-03-13 15:28:56 -0300

    :zap: Implementação de busca por Id de tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,12 @@ public class TopicosController {
 			return TopicoDTO.converte(topicos);
 		}
 		
+	}
+	
+	@GetMapping("/{id}")
+	public TopicoDTO detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new TopicoDTO(topico);
 	}
 	
 	@PostMapping
